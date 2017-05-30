68ad94d7674c4f1126b0e7593dc057994c35f5d4
himaaaatti <sux2mfgj@gmail.com>
2017-05-30 23:14:25 +0900

    Set constraint value to pci configuration space for NVMe. This emulator can be detected by guest FreeBSD, but occured panic.

--- usr.sbin/bhyve/pci_nvme.c
+++ usr.sbin/bhyve/pci_nvme.c
@@ -1,6 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "pci_emul.h"
+
+#define NVME_DEBUG
+
+#ifdef NVME_DEBUG
+static FILE *dbg;
+#define DPRINTF(format, arg...)	do{fprintf(dbg, format, ##arg);fflush(dbg);}while(0)
+#else
+#define DPRINTF(format, arg...)
+#endif
+
+struct pci_nvme_softc {
+
+    uint32_t cap_hi;
+    uint32_t cap_low;
+    uint32_t vs;
+    uint32_t intms;
+    uint32_t intmc;
+    uint32_t cc;
+    uint32_t reserved0;
+    uint32_t csts;
+    uint32_t nssr;
+    uint32_t aqa;
+    uint64_t asq;
+    uint64_t acq;
+    uint32_t cmbloc;
+    uint32_t cmbsz;
+    uint32_t bpinfo;
+    uint32_t bprsel;
+    uint64_t bpmbl;
+
+    //
+    uint32_t sqytdbl;
+    uint32_t cqyhdbl;
+
+
+};
+
+static void
+nvme_reset(struct pci_nvme_softc *sc)
+{
+    sc->nssr = 0x4e564d65;
+}
+
+static int 
+pci_nvme_init (struct vmctx *ctx, struct pci_devinst *pi, char *opts)
+{
+    struct pci_nvme_softc *sc;
+
+#ifdef NVME_DEBUG
+	dbg = fopen("/tmp/nvme_emu_log", "w+");
+#endif
+
+    pci_set_cfgdata16(pi, PCIR_DEVICE, 0x0111);
+    pci_set_cfgdata16(pi, PCIR_VENDOR, 0x8086);
+
+	sc = calloc(1, sizeof(struct pci_nvme_softc));
+	pi->pi_arg = sc;
+/*     sc->asc_pi = pi; */
+
+    nvme_reset(sc);
+
+    return 0;
+}
+
+static void
+pci_nvme_write(struct vmctx *ctx, int vcpu, struct pci_devinst *pi,
+		int baridx, uint64_t offset, int size, uint64_t value)
+{
+
+}
+
+static uint64_t
+pci_nvme_read(struct vmctx *ctx, int vcpu, struct pci_devinst *pi, int baridx,
+    uint64_t regoff, int size)
+{
+
+    DPRINTF("0x%lx",  regoff);
+    return 0;
+}
 
 struct pci_devemu pci_de_nvme = {
     .pe_emu = "nvme",
+    .pe_init = pci_nvme_init,
+    .pe_barwrite = pci_nvme_write,
+    .pe_barread = pci_nvme_read
 };
 PCI_EMUL_SET(pci_de_nvme);
